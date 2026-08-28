04d25acf9963a7f986dcb673d527f0b3ceb4a4ea
moigon2408xp-pixel <moigon2408xp@gmail.com>
2026-08-28 02:05:11 -0400

    Actualizacion por carpeta de drive

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 /* Reemplaza el valor por la URL /exec de la aplicación web de Apps Script. */
-window.PRIORIDAD_CONFIG = { appsScriptUrl: "https://script.google.com/macros/s/AKfycby_mIt5VzEOZjKb6znpYXH_T0Q0jJfEqr5UB1Z8l0JpUiHfEC9CuRuK9z2s_Q3lNl6www/exec" };
+window.PRIORIDAD_CONFIG = { appsScriptUrl: "https://script.google.com/macros/library/d/1ngT52695pxqn4JF7ZuIJ_7wk6dvm46cVEOP3gOiKXc3cw6D0ToWP96po/11" };
