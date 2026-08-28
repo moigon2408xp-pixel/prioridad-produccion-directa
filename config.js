44c6786a659c058707b295d5988e80dbf16bfe6f
moigon2408xp-pixel <moigon2408xp@gmail.com>
2026-08-28 01:43:51 -0400

    Conectar PWA con cuenta secundaria

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 /* Reemplaza el valor por la URL /exec de la aplicación web de Apps Script. */
-window.PRIORIDAD_CONFIG = { appsScriptUrl: "https://script.google.com/macros/s/AKfycbwttZ7YFaptEmDyYiHMIjSl_K-Nv5HTP2X8CW_NPzzS2gfmo2mYcigX-XSOprHkMDa5eQ/exec" };
+window.PRIORIDAD_CONFIG = { appsScriptUrl: "https://script.google.com/macros/s/AKfycby_mIt5VzEOZjKb6znpYXH_T0Q0jJfEqr5UB1Z8l0JpUiHfEC9CuRuK9z2s_Q3lNl6www/exec" };
