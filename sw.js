cb514810c32c12cfc13d7254e56a5bcd761bfdb0
moigon2408xp-pixel <moigon2408xp@gmail.com>
2026-08-27 23:59:32 -0400

    Cargar formulario de alta sin caché antigua

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "prioridad-produccion-perfiles-v8";
-const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
+const CACHE = "prioridad-produccion-perfiles-v9";
+const ASSETS = ["./","./index.html","./styles.css","./app-registrar-fix.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 try {
   importScripts("./firebase-config.js");
   const config = self.PRIORIDAD_FIREBASE_CONFIG || {};
