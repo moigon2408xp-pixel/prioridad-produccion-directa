e7a3c85c7c89c9d416f4aeec49218485706a5322
moigon2408xp-pixel <moigon2408xp@gmail.com>
2026-08-31 05:53:09 -0400

    Update delivery time options in form

--- app-registrar-fix.js
+++ app-registrar-fix.js
@@ -641,9 +641,21 @@ function formOrder() {
         <label class="field"><span class="field-label">FECHA DE ENTREGA</span><input type="date" id="input-fecha" name="fechaEntrega" required></label>
         <label class="field"><span class="field-label">HORA DE ENTREGA</span>
           <select name="horaEntrega" required>
-            <option value="11:00 AM" selected>11:00 AM</option>
-            <option value="02:00 PM">02:00 PM</option>
-            <option value="05:00 PM">05:00 PM</option>
+            <option value="07:00">07:00 AM</option>
+            <option value="08:00">08:00 AM</option>
+            <option value="09:00">09:00 AM</option>
+            <option value="10:00">10:00 AM</option>
+            <option value="11:00" selected>11:00 AM</option>
+            <option value="12:00">12:00 PM</option>
+            <option value="13:00">01:00 PM</option>
+            <option value="14:00">02:00 PM</option>
+            <option value="15:00">03:00 PM</option>
+            <option value="16:00">04:00 PM</option>
+            <option value="17:00">05:00 PM</option>
+            <option value="18:00">06:00 PM</option>
+            <option value="19:00">07:00 PM</option>
+            <option value="20:00">08:00 PM</option>
+            <option value="21:00">09:00 PM</option>
           </select>
         </label>
       </div>
